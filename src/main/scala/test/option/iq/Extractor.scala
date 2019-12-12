a63c095334b2190c97f09a148d90aada5a1445c3
osharnikov <oleg.sharnikov@kohls.com>
2019-12-12 22:48:50 +0300

    Insert new vacancies

--- src/main/scala/test/option/iq/Extractor.scala
+++ src/main/scala/test/option/iq/Extractor.scala
@@ -4,6 +4,8 @@ import java.util.Properties
 
 import SchemaFactory.getRawSchema
 import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.functions.col
+
 
 
 object Extractor extends App {
@@ -14,50 +16,73 @@ object Extractor extends App {
     .appName("extractor")
     .getOrCreate()
 
+  import sparkSession.sqlContext.implicits._
+
 //  sparkSession.sparkContext.textFile("hdfs://172.17.0.2:9000/data.csv").foreach(println)
 //  val file = sparkSession.sparkContext.textFile("data.csv").foreach(println)
 
     val databaseUrl = "jdbc:postgresql://localhost:5432/postgres"
-
-    val newData = sparkSession.read
-      .option("sep", ";")
-      .schema(getRawSchema())
-      .csv("/home/osharnikov/IdeaProjects/DataFetcher/data.csv")
-
-    val dataToSave = newData.select(
-      "id",
-      "premium",
-      "name",
-      "department_id",
-      "department_name",
-      "has_test",
-      "response_letter_required",
-      "area_id",
-      "area_name",
-      "salary_from",
-      "salary_to",
-      "salary_currency",
-      "salary_gross",
-      "employer_id",
-      "employer_name",
-      "created_at",
-      "url",
-      "alternate_url",
-      "snippet_requirement",
-      "snippet_responsibility"
-    )
-
-    dataToSave.show()
 
     val connectionProperties = new Properties()
     connectionProperties.put("Driver", "org.postgresql.Driver")
     connectionProperties.put("user", "postgres")
     connectionProperties.put("password", "PGPASSWORD")
 
-  dataToSave
-      .write
-      .mode(SaveMode.Append)
-      .jdbc(databaseUrl, "vacancies", connectionProperties)
+    val loadedData = sparkSession.read
+      .option("sep", ";")
+      .schema(getRawSchema())
+      .csv("/home/osharnikov/IdeaProjects/DataFetcher/data.csv")
+
+    if (loadedData.count() > 0) {
+      val loadedVacancies = loadedData.select(
+        "id",
+        "premium",
+        "name",
+        "department_id",
+        "department_name",
+        "has_test",
+        "response_letter_required",
+        "area_id",
+        "area_name",
+        "salary_from",
+        "salary_to",
+        "salary_currency",
+        "salary_gross",
+        "employer_id",
+        "employer_name",
+        "created_at",
+        "url",
+        "alternate_url",
+        "snippet_requirement",
+        "snippet_responsibility"
+      )
+
+//      loadedVacancies.show()
+
+      val openIds = sparkSession.read.jdbc(databaseUrl, "vacancies", connectionProperties)
+        .select("id")
+        .where(col("is_open").equalTo(true))
+
+      val openIdsList = openIds.collect().map(_(0)).toList
+
+      val newCacancies = loadedVacancies.where(!col("id").isin(openIdsList:_*))
+      newCacancies.write
+        .mode(SaveMode.Append)
+        .jdbc(databaseUrl, "vacancies", connectionProperties)
+
+      newCacancies.show()
+//      val deleteVacanciesIds = openIds.where()
+    }
+
+
+
+
+
+
+//    vacanciesToSave
+//      .write
+//      .mode(SaveMode.Append)
+//      .jdbc(databaseUrl, "vacancies", connectionProperties)
 
 
 //    val df = sparkSession.read.jdbc(databaseUrl, "(select * from vacancies) as vac", connectionProperties)
